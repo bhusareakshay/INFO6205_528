6cb067c8f1d11f51249763cd548916052c7b1c22
rishabhbjain <jain.rishabh@husky.neu.edu>
2019-04-18 01:52:45 -0400

    infinity check fitness

--- JobShopGeneticAlgorithm/src/test/java/psa/fitness/fitnessTest.java
+++ JobShopGeneticAlgorithm/src/test/java/psa/fitness/fitnessTest.java
@@ -1,9 +1,11 @@
 package psa.fitness;
 
 import static org.junit.Assert.assertEquals;
+import psa.expression.*;
 
 import org.junit.Test;
 
+import psa.expression.ExpressionGeneration;
 import psa.helper.Generator;
 import psa.model.Generation;
 import psa.system.Candidate;
@@ -16,6 +18,8 @@ public class fitnessTest {
 		Generator.getInstance();
 		Population p = Population.getInstance();
 		p.createGeneration0();
+		ExpressionGeneration exp = new ExpressionGeneration();
+		exp.CalculateExpression();
 		FitnessCalculation fc = new FitnessCalculation();
 		fc.fitness();
 		Generation g = p.getGenerationMap().get(0);
@@ -26,6 +30,29 @@ public class fitnessTest {
 			}
 		}
 		assertEquals(true,max);	
+	}
+	
+	@Test
+	public void TestInfinity() {
+		Population p = Population.getInstance();
+		p.createGeneration0();
+		ExpressionGeneration exp = new ExpressionGeneration();
+		exp.CalculateExpression();
+		FitnessCalculation fc = new FitnessCalculation();
+		fc.fitness();
+		Generation g = p.getGenerationMap().get(0);
+		boolean infinite = false;
+		for(Candidate c:g.getCandidateList()) {
+			try{
+				double inv = 1/c.gettMax();
+				if( 1/inv == 0.0){
+					infinite = true;
+				}
+			}catch(Exception e) {
+				
+			}
+		}
+		assertEquals(false,infinite);	
 	}
 	
 }
